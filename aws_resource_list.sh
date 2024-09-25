a1838b90244e568529d289eb83d693534cf5948f
Sasank77 <100476506+Sasank77@users.noreply.github.com>
2024-09-25 14:51:11 +0530

    Update aws_resource_list.sh

--- aws_resource_list.sh
+++ aws_resource_list.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ###############################################################################
-# Author: Abhishek Veeramalla
+# Author: sasank
 # Version: v0.0.1
 
 # Script to automate the process of listing all the resources in an AWS account
